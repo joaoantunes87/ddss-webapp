68eab81e7d376d865990dc110ee9d3254975dac6
João Antunes <joaoantunes87@gmail.com>
2019-09-08 09:36:18 +0100

    Seed for database, opening for vulnerabilities, logs

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # ddss-webapp
+
 DDSS: Unsecure Web App
 
 # Docker
+
 ### Start Web Application
+
 **Note: Port 3000 must not being used**
 
 ```sh
 docker build -t ddss-webapp .
-```
-
-```sh
-cd postgres-database-docker && docker build -t ddss-database .
 ```
 
 ```sh
@@ -24,9 +23,11 @@ cd .. && docker-compose up
 Go to http://localhost:3000
 
 ### Stop Web Application
+
 ```sh
 docker-compose down
 ```
 
 ### Persistent Database
+
 // TODO Configure Volume
--- postgres-database-docker/init-db.sql
+++ postgres-database-docker/init-db.sql
@@ -1,3 +1,4 @@
+/* schema */
 DROP TABLE IF EXISTS ddss_user;
 CREATE TABLE ddss_user(
    email VARCHAR (50) PRIMARY KEY,
@@ -19,3 +20,16 @@ CREATE TABLE payment(
    validity VARCHAR (5),
    security_code VARCHAR (3)
 );
+
+/* seeds */
+INSERT INTO ddss_user
+VALUES ('jcfa@dei.pt', '123456', 'João');
+
+INSERT INTO ddss_user
+VALUES ('jcfa+2@dei.pt', '123456', 'João 002');
+
+INSERT INTO payment (user_email, card_number, validity, security_code)
+VALUES ('jcfa@dei.pt', '4444 3333 2222 1111', '05/22', '123');
+
+INSERT INTO payment (user_email, card_number, validity, security_code)
+VALUES ('jcfa+2@dei.pt', '9999 8888 7777 6666', '12/22', '987');
